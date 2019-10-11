f4aea2815504728896db7c2146a09c32cdf3bcd0
Bruno Balbueno <brunobalbuenom@gmail.com>
2019-10-11 18:02:48 -0300

    Initial commit

---
+++ Assets/Script/DogState.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogState : MonoBehaviour
+{
+    bool _hungry;
+    [SerializeField]
+    Animator _myAnimator;
+    [SerializeField]
+    GameObject _myUrgentBaloon;
+    [SerializeField]
+    GameObject _myBaloon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _hungry = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!_hungry)
+        {
+            _myUrgentBaloon.SetActive(false);
+            _myAnimator.Play("Happy");
+        }
+        else
+        {
+            _myUrgentBaloon.SetActive(true);
+        }
+    }
+
+    public void SetHungry(bool p_hugry)
+    {
+        _hungry = p_hugry;
+    }
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public SpriteRenderer bowl;
+    public DogState dogState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            bowl.sprite = Resources.Load<Sprite>("Sprite/bowl");
+            dogState.SetHungry(false);
+        }
+    }
+}
