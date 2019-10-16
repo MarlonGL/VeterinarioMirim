1ff8c3c58a49dbefcab45b2fb956c0055a5175dd
Bruno Balbueno <brunobalbuenom@gmail.com>
2019-10-15 22:41:45 -0300

    Drag

---
+++ Assets/Script/Slot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public enum ItemType
+{
+    SHOWER,
+    FOOD,
+    PULGA
+}
+
+public class Slot : MonoBehaviour, IDropHandler
+{
+    public ItemType type;
+    public GameObject item
+    {
+        get
+        {
+            if (transform.childCount > 0)
+                return transform.GetChild(0).gameObject;
+            else
+                return null;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (!item)
+        {
+            if (DragHandler.itemBeingDragged.GetComponent<DragHandler>().type == type)
+            {
+                DragHandler.itemBeingDragged.transform.SetParent(transform);
+                DragHandler.itemBeingDragged.transform.transform.position = transform.position;
+            }
+        }
+    }
+}
